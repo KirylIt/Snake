dad5dab4324e72c1a90950a95144353a0affdfa8
Kiryl Kunavich <kirill99hockey@gmail.com>
2023-04-28 21:47:07 +0300

    3apple1.1

--- src/GameField.java
+++ src/GameField.java
@@ -53,25 +53,56 @@ public class GameField extends JPanel {
             y[i] = 48;  // координаты змейки на начало игры по У
             x[i] = 48 - i * DOT_SIZE; // расположение горизонтально в левую сторону
         }
-        timer = new Timer(150,this);
+        timer = new Timer(150, this);
         timer.start();
         createApple();
     }
 
-    public void chekApple(){ // проверка съела ли змейка наше яблоко и добавление очков
-        if (x[0] == appleX && y[0] == appleY){ // 1 яблоко
+    public void chekApple() { // проверка съела ли змейка наше яблоко и добавление очков
+        if (x[0] == appleX && y[0] == appleY) { // 1 яблоко
             dots++;
             createApple();
         }
 
-        if (x[0] == appleX1 && y[0] == appleY1){ // 2 яблоко
+        if (x[0] == appleX1 && y[0] == appleY1) { // 2 яблоко
             dots++;
             createApple();
         }
 
-        if (x[0] == appleX2 && y[0] == appleY2){ // 3 яблоко
+        if (x[0] == appleX2 && y[0] == appleY2) { // 3 яблоко
             dots++;
             createApple();
         }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) { // отрисовка
+        super.paintComponent(g);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot, x[i], y[i], this);
+            }
+        } else {
+            String str = "Game Over";
+            g.setColor(Color.CYAN);
+            g.drawString(str, SIZE / 6, SIZE / 2);
+        }
+    }
+
+    public void chekCollision() { // проверка на врезание в стенку или в саму себя
+        for (int i = 0; i < dots; i++) {
+            if (x[0] == x[i] && y[0] == y[i]) {
+                inGame = false; // конец игры!
+            }
+        }
+        if (x[0] > SIZE) // проверка при достижении змейки правой стенки (переход в левую стенку)
+            x[0] = 0;
+        if (x[0] > 0) // проверка при достижении змейки левой стенки (переход в правую стенку)
+            x[0] = SIZE;
+        if (y[0] > SIZE) // проверка при достижении змейки верхней стенки (переход в низ)
+            y[0] = 0;
+        if (y[0] > 0) // проверка при достижении змейки нижней стенки (переход в верх)
+            y[0] = SIZE;
     }
 }
